69e7804c9222d346ef7e81342881bf62921cfabb
Nishant Rodrigues <nishantjr@gmail.com>
2017-04-27 18:58:03 -0500

    After much weeping and gnashing of teeth, git-ci can use libgit2

--- git-ci.go
+++ git-ci.go
@@ -1,7 +1,18 @@
 package main
 
 import "fmt"
+import "git"
+import "os"
 
 func main() {
-	fmt.Println("Ha ha ha!")
+    repo, err := git.OpenRepository(".")
+    if err != nil {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+    }
+
+    head,   err := repo.References.Lookup("HEAD")
+    object, err := head.Peel(git.ObjectCommit)
+    commit, err := object.AsCommit()
+    fmt.Printf("%s is the best.\n", commit.Author().Name)
 }
