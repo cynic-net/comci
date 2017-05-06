83603318fcf9063c8ea7d486e822c3b03f937643
Curt J. Sampson <cjs@cynic.net>
2017-05-06 11:43:52 +0900

    Main: Open a repo and print the HEAD ref etc.
    All the obvious example code bits and tutorials are competely
    non-working. So we came up with this ourselves.
    
    It seems that the `gitlib` package is basically just an interface, and
    relies on another package to provide a Git.RepositoryFactory on which
    `gitlib` can work. In this case we use Git.Libgit2.lgFactory from the
    `gitlib-libgit2` package to create the RepositoryFactory.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
+{-# Language OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Control.Monad.IO.Class
+import Git
+import Git.Libgit2
 
 main :: IO ()
-main = someFunc
+main =
+    withRepository' lgFactory opts $ do
+        f <- facts;                         liftIO $ print f
+        ref <- lookupReference "HEAD";      liftIO $ print ref
+        return ()
+    where
+        opts = RepositoryOptions
+            { repoPath = "test/fixture/repo00.git"
+            , repoWorkingDir = Nothing
+            , repoIsBare = True
+            , repoAutoCreate = False
+            }
+
+instance Show (RefTarget r) where
+    show (RefObj oid) = "RefObj#" ++ undefined -- show (renderOid oid)
+    show (RefSymbolic name) = "RefSymbolic#" ++ show name
