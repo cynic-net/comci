db6ec79c45d24ea908e69a482e5914379ca0d2f7
Nishant Rodrigues <nishantjr@gmail.com>
2017-05-16 11:37:01 -0500

    app/Main.hs: extract commit SHA, message and tree SHA from ref

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,23 @@ module Main where
 import Control.Monad.IO.Class
 import Git
 import Git.Libgit2
+import Data.Maybe
+import Data.Text
 
 main :: IO ()
 main =
     withRepository' lgFactory opts $ do
-        f <- facts;                         liftIO $ print f
-        ref <- lookupReference "HEAD";      liftIO $ print ref
+        mRef <- resolveReference "HEAD"
+        let ref = fromMaybe (error "HEAD doesn't exist") mRef
+        commitOid <- parseObjOid $ renderOid ref
+        commit <- lookupCommit commitOid
+        tree <- lookupTree $ commitTree commit
+        treeOid <- treeOid tree
+
+        liftIO $ putStrLn $
+                 "commit: "  ++ (show commitOid)
+            ++ "\nmessage: " ++ (unpack $ commitLog commit)
+            ++   "tree: "    ++ (show treeOid)
         return ()
     where
         opts = RepositoryOptions
