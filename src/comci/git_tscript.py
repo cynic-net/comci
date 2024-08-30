5c931c4db02537ef7d1e75c3a104673914cc7515
Curt J. Sampson <cjs@cynic.net>
2024-08-30 14:10:02 +0000

    FAILING comc.git_tscript: start testing subparsed commands

--- src/comci/git_tscript.py
+++ src/comci/git_tscript.py
@@ -15,11 +15,17 @@ same results.
 
 '''
 from    argparse  import ArgumentParser, Namespace
+from    functools  import partial as par
 from    os  import chdir, getcwd
 from    pathlib  import Path
 from    stat  import S_IXUSR
 from    subprocess  import run, DEVNULL
-from    sys  import argv, stdout, stderr
+
+#   Pytest replaces `sys.stdout` and `sys.stderr` for the duration of
+#   a test when we're using the `capsys` fixture, so we need to read the
+#   value from `sys` _after_ the test has started, rather than when we're
+#   imported at collection time.
+import  sys
 
 #   XXX A `py.typed` was added to pygit2 in the commit just after the
 #   1.15.1 release. The type:ignore below should be removed as soon
@@ -34,23 +40,26 @@ from    pygit2  import (Repository, discover_repository)
 ####################################################################
 #   Main
 
-ARGS            :Namespace
 PROJECT_ROOT    :Path
 REPO            :Repository
 
-def main():
-    global ARGS, PROJECT_ROOT, REPO
-    ARGS = parseargs(argv[1:])
+def main(command_line_args=None):
+    global PROJECT_ROOT, REPO
+    args = parseargs(command_line_args)
     REPO = find_repo()
     PROJECT_ROOT = Path(REPO.path).parent
     chdir(PROJECT_ROOT)
+    args.cmd(args)
+
+def command_run(args):
+    die(9, 'XXX Write me!')
     for ts in tscripts(PROJECT_ROOT):
         #   XXX in both cases here we should be dealing with the optional
         #   single argument to the test(s) somehow?
-        if ARGS.interactive:
+        if args.interactive:
             run_ts_interactive(ts, arg=None)
         else:
-            run_ts_capture(ts, arg=None, foreground=ARGS.foreground)
+            run_ts_capture(ts, arg=None, foreground=args.foreground)
 
 def parseargs(command_line_args):
     top_parser = ArgumentParser(description='''
@@ -60,6 +69,7 @@ def parseargs(command_line_args):
     sub = top_parser.add_subparsers(required=True)
 
     run_parser = sub.add_parser('run', help='XXX run')
+    run_parser.set_defaults(cmd=command_run)
     ra = run_parser.add_argument
     ra('-R', '--rerun', action='store_true',
         help='Run tests on a commit even when we already have test results stored')
@@ -75,10 +85,15 @@ def parseargs(command_line_args):
         help='0 or more `tname`, `tname,param,param`')
 
     show_parser = sub.add_parser('show', help='XXX show')
+    show_parser.set_defaults(cmd=par(unimpl_command, 'show'))
 
     list_parser = sub.add_parser('list', help='XXX list')
+    list_parser.set_defaults(cmd=par(unimpl_command, 'show'))
 
     return top_parser.parse_args(command_line_args)
+
+def unimpl_command(name, args):
+    die(9, f'unimplemented command: {name}')
 
 ####################################################################
 #   Run single tests.
@@ -115,7 +130,7 @@ def run_ts_capture(ts, arg=None, foreground=False):
     errpath = output_path(ts, arg, 'err')
 
     ec = run_ts(ts, arg, io=(outpath, errpath))
-    fprint(stdout, 'git-tscript: {}{} completed (exit={})'.format(
+    fprint(sys.stdout, 'git-tscript: {}{} completed (exit={})'.format(
         ts.name, ' ' + arg if arg else '', ec))
 
 def output_path(ts, arg, suffix):
@@ -148,7 +163,7 @@ def run_ts(ts, arg=None, inherit_stdin=False, io=(None, None)):
     iof = tuple(openf(i) for i in io )
 
     header_fd = iof[0];
-    if header_fd is None: header_fd = stdout
+    if header_fd is None: header_fd = sys.stdout
     if arg is None:
         command = [ts]
         fprint(header_fd, f'━━━━━━━━━━━━ {ts.name}')
@@ -187,12 +202,12 @@ def tscripts(project_root):
 
 def debug(*s):
     return
-    print(*s, file=stderr, flush=True)
+    print(*s, file=sys.stderr, flush=True)
 
 def fprint(fd, *args):
     print(*args, file=fd, flush=True)
 
 def die(exitcode, *s):
-    print(*s, file=stderr, flush=True)
-    print( 'XXX exitcode:', exitcode, file=stderr, flush=True)
+    print(*s, file=sys.stderr, flush=True)
+    print( 'XXX exitcode:', exitcode, file=sys.stderr, flush=True)
     exit(exitcode)
