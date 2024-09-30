add0cb18fd8a7ea23b297867f15dca5974a78eb2
Curt J. Sampson <cjs@cynic.net>
2024-09-30 13:42:03 +0000

    WIP run: Start to work out background process handling

--- src/comci/run.py
+++ src/comci/run.py
@@ -13,7 +13,7 @@ import  sys
 from    comci.util  import debug, die, fprint
 import  comci.util as util
 
-ParamType = str | None
+ParamType = str
 
 ####################################################################
 
@@ -21,31 +21,49 @@ def command_run(args):
     ''' Run the testspecs (test names and parameters) given on the command
         line or, if none given, all the tests with their default parameters.
     '''
+
+    #   • Get worktree
+    #     Use working copy if commit is ``.``, otherwise clone new
+    #     working copy for given commit.
+    #   • Get test specs
+    #     List of pairs of (tscript,param)
+    #   • Get capture params (set up capture mode?)
+    #     To stdout/err, file, commit.
+
+
     for ts in tscripts(util.PROJECT_ROOT):
         #   XXX in both cases here we should be dealing with the optional
         #   single argument to the test(s) somehow?
         if args.interactive:
-            run_ts_interactive(ts, param=None)
-        else:
-            run_ts_capture(ts, param=None, foreground=args.foreground)
-
-####################################################################
-#   Run single tests.
-
-def run_ts_interactive(ts:Path, param:ParamType=None):
     ''' Run test scripts in "interactive" mode, waiting for tests to complete
         and stopping at the first failure. These are run against the actual
         working tree with the stdin/stdout/stderr of this process. The exit
         code is 0 for success or the exit code of the test script that failed.
     '''
-    ec = run_ts(ts, param, inherit_stdin=True)
-    if ec != 0: exit(ec)
-
-def run_ts_capture(ts:Path, param:ParamType=None, foreground=False):
+            run_ts_interactive(ts, param='')
+        else:
     ''' Run all test scripts in "background" mode, capturing output and
         and failure status of each one. These are run against a copy of
         the working tree.
+    '''
+            run_ts_capture(ts, param='', foreground=args.foreground)
 
+####################################################################
+#   Run single tests.
+
+def run_ts_interactive(ts:Path, param:ParamType):
+    '''
+        • Foreground
+        • Not redirect output
+        • Not parallel?
+        • Do not use fresh work tree
+    '''
+    ec = run_ts(ts, param, inherit_stdin=True)
+    if ec != 0: exit(ec)
+
+def run_ts_capture(ts:Path, param:ParamType, foreground=False):
+    '''
+        XXX works against a particular commit?
         XXX capture output
 
         • stdin is always ``/dev/null``.
@@ -67,21 +85,13 @@ def run_ts_capture(ts:Path, param:ParamType=None, foreground=False):
     fprint(sys.stdout, 'git-tscript: {}{} completed (exit={})'.format(
         ts.name, ' ' + param if param else '', ec))
 
-def output_path(ts:Path, param:ParamType, suffix:str) -> Path:
-    '''
-        • `ts`: `Path` to the test script under ``tscript/``
-        • `param`: test parameter (optional)
-        • `suffix`: ``.out`` or ``.err``
-    '''
-    if param is None: param = ''
-    fname = '.'.join([ts.name, param, suffix])
-    return util.PROJECT_ROOT / '.build' / 'tscript' / 'out' / fname
+def run_ts(ts:Path, param:ParamType, inherit_stdin=False, io=(None, None)):
+    ''' Run the test script at path `ts`, with test parameter `param`.
+        A test header will be logged to the standard output (whether
+        inherited or captured), and also a failure message if the `ts` exit
+        code is anything other than 0.
 
-def run_ts(ts, arg=None, inherit_stdin=False, io=(None, None)):
-    ''' Run the test script at path `ts`, with optional argument `arg`
-        (default `None`). A test header will be logged to the standard
-        output (whether inherited or captured), and also a failure message
-        if the `ts` exit code is anything other than 0.
+        * XXX Expects `cd` to correct place....
 
         * `inherit_stdin` is either `True` to let the tscript read from
           this process' `stdin`, or `False` to have `stdin` return EOF on
@@ -103,12 +113,12 @@ def run_ts(ts, arg=None, inherit_stdin=False, io=(None, None)):
 
     header_fd = iof[0];
     if header_fd is None: header_fd = sys.stdout
-    if arg is None:
-        command = [ts]
+    if param == '':
+        command:list[str|Path] = [ts]
         fprint(header_fd, f'━━━━━━━━━━━━ {ts.name}')
     else:
-        command = [ts, arg]
-        fprint(header_fd, f'━━━━━━━━━━━━ {ts.name} {arg}')
+        command = [ts, param]
+        fprint(header_fd, f'━━━━━━━━━━━━ {ts.name} {param}')
 
     debug('io:', io)
     stdin = None if inherit_stdin else DEVNULL
@@ -117,6 +127,15 @@ def run_ts(ts, arg=None, inherit_stdin=False, io=(None, None)):
     for i in iof:
         if i is not None: i.close()
     return ec
+
+def output_path(ts:Path, param:ParamType, suffix:str) -> Path:
+    '''
+        • `ts`: `Path` to the test script under ``tscript/``
+        • `param`: test parameter (optional)
+        • `suffix`: ``.out`` or ``.err``
+    '''
+    fname = '.'.join([ts.name, param, suffix])
+    return util.PROJECT_ROOT / '.build' / 'tscript' / 'out' / fname
 
 def tscripts(project_root):
     for pts in sorted(project_root.glob('tscript/*')):
